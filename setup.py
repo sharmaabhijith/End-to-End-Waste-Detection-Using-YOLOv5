998de5e21ba2a52607d9a6b85abac82d7924efae
sharmaabhijith <sharmaabhijith1@gmail.com>
2024-01-07 18:54:55 -0500

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Abhijith Sharma',
+    author_email= 'sharmaabhijith1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
